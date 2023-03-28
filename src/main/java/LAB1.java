f90e5c6c3019ff48f813c4d5e76ed8b843b3a77f
Niraphan <niraphan2560@gmail.com>
2023-03-28 13:27:42 +0700

    Finishing lab

--- src/main/java/LAB1.java
+++ src/main/java/LAB1.java
@@ -1,23 +1,14 @@
 public class LAB1 {
     public static void main(String[] args) {
-//        int[] list = new int[10];
-//        try {
-//            System.out.println("list[10] is" list[10] );
-//        }
-//        catch (ArithmeticException ex) {
-//            System.out.println("ArithmeticException");
-//        }
-//        catch (RuntimeException ex) {
-//            System.out.println("RuntimeException");
-//        }
-//        catch (Exception ex) {
-//            System.out.println("exception");
-//        }
-        System.out.println(quotient(5,0));
-}public static int quotient(int number1, int number2) {
-    if (number2 == 0)
-        throw new ArithmeticException("Divisor cannot be zero");
-    return number1 / number2;
-
-}
+//        System.out.println(1/0);
+//        int[] list = new int[5];
+//        System.out.println(list[5]);
+//        String s = "abc";
+//        System.out.println(s.charAt(3));
+//        Object o = new Object();
+//        String s = (String)o;
+//        Object o = null;
+//        System.out.println(o.toString());
+//        System.out.println(1.0/0);
+    }
 }
---
+++ src/main/java/lab3.java
@@ -0,0 +1,16 @@
+public class lab3 {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Statement1");
+            System.out.println(1/0);
+            System.out.println("Statement3");
+        }
+        catch (ArithmeticException e) {
+        }
+        finally {
+            System.out.println("Statement 4");
+        }
+        System.out.println("statement 5");
+
+    }
